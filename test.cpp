23e385b7179c5121b65be240f3950e9aa0a0f1c4
Ricky <rjromanach@gmail.com>
2022-02-05 14:05:52 -0600

    added interaction
    test with merge conflict stuff

--- test.cpp
+++ test.cpp
@@ -5,7 +5,18 @@ using namespace std;
 
 int main()
 {
+    bool hello = true;
     cout << "Hello world!" << endl;
+    
+    if (hello)
+    {
+        cout << "Hi" << endl;
+    }
+    else
+    {
+        cout << "Bye" << endl;
+    }
+	
 
     cout << "\n ('_') \n";
 
